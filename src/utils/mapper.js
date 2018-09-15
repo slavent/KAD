78700458b408e95dffa0d794a4722427cd6eb552
Slavent <startupne@gmail.com>
2018-09-15 13:13:17 +0300

    refactoring

--- src/utils/mapper.js
+++ src/utils/mapper.js
@@ -1,9 +1,4 @@
 /**
- * @author Kozinets Svyatoslav
- */
-import holidays from "data/holidays"
-
-/**
  * маппинг всех постов
  */
 export function mapPosts( data ) {
@@ -14,35 +9,6 @@ export function mapPosts( data ) {
 
         data.id = item.id
         newData.push( data )
-    } )
-
-    return newData
-}
-
-/**
- * маппинг поста
- */
-export function mapPost( data ) {
-    return _.pick( data, "acf" ).acf
-}
-
-/**
- * маппинг данных для раздела "Обо мне"
- */
-export function mapAboutMe( data ) {
-    return _.pick( _.pick( data, "acf" ).acf, "photo", "desc" )
-}
-
-/**
- * маппинг альбомов
- */
-export function mapPhotos( data ) {
-    let newData = []
-
-    _.filter( data, item => item.categories[ 0 ] === 3 ).map( ( item, i ) => {
-        let newItem = _.pick( item, "acf" ).acf
-        newItem.id = item.id
-        newData.push( newItem )
     } )
 
     return newData
@@ -67,31 +33,6 @@ export function mapPortfolio( data ) {
 }
 
 /**
- * маппинг праздников и дней рождений
- */
-export function mapDataForDatepicker( data ) {
-    let dataMapped = []
-    data = _.filter( data, item => item.categories[ 0 ] === 8 )
-
-    data.map( ( item, i ) => {
-        dataMapped.push( _.pick( item, "acf" ).acf )
-    } )
-
-    holidays.map( ( item, i ) => {
-        dataMapped.push( item )
-    } )
-
-    return dataMapped
-}
-
-/**
- * маппинг опроса
- */
-export function mapPoll( data ) {
-    console.log( data )
-}
-
-/**
  * маппинг расписания
  */
 export function mapSheduler( data ) {
@@ -103,36 +44,6 @@ export function mapSheduler( data ) {
  */
 export function mapHolidays( data ) {
     return _.pick( _.pick( data, "acf" ).acf, "pic" ).pic
-}
-
-/**
- * маппинг поздравлений
- */
-export function mapCongratulations( data ) {
-    let newData = []
-
-    _.map( data, ( item, i ) => {
-        let newItem = _.pick( _.pick( item, "acf" ).acf, "title", "desc" )
-        newItem.id = item.id
-        newData.push( newItem )
-    } )
-
-    return newData
-}
-
-/**
- * маппинг объявлений
- */
-export function mapAds( data ) {
-    let newData = []
-
-    _.map( data, ( item, i ) => {
-        let newItem = _.pick( _.pick( item, "acf" ).acf, "title", "desc" )
-        newItem.id = item.id
-        newData.push( newItem )
-    } )
-
-    return newData
 }
 
 /**
