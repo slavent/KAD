8a656f5c8dd75e4e636075a1483cbf48c804fcf7
Slavent <startupne@gmail.com>
2018-09-15 13:18:32 +0300

    refactoring

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,8 @@
 const ROOT = "peremenka20.ru"
 
-const API = {
+export default {
     GET_POSTS: "http://" + ROOT + "/wp-json/acf/v3/posts?categories=2",
     GET_POST: "http://" + ROOT + "/wp-json/acf/v3/posts?categories=2",
     GET_MESSAGES: "http://" + ROOT + "/wp-json/acf/v3/posts?categories=4",
     GET_GALLERY: "http://" + ROOT + "/wp-json/acf/v3/posts?categories=3"
 }
-
-export default API
